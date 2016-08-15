e2177bcf959deb623825764590fb8c0eac0b304e
pssguy <agcur@rogers.com>
2016-08-15 10:09:25 -0700

    added updated packages started with individual/updates default with newest over 7 days ranomized individual to one of top 50

--- dailyUpdate.R
+++ dailyUpdate.R
@@ -2,6 +2,7 @@ library(rvest)
 library(cranlogs)
 library(feather)
 library(readr)
+library(stringr)
 library(dplyr)
 
 info <- read_html("https://cran.r-project.org/web/packages/available_packages_by_name.html#available-packages-A")
@@ -55,5 +56,89 @@ df <- rbind(df,tot)
 write_feather(df,"data/allCranLogs.feather")
 write_csv(cran_df,"data/packages.csv")
 
+## updated packages
+
+updates <- read_html("http://dirk.eddelbuettel.com/cranberries/cran/updated/")
+
+package <- updates %>% 
+  html_nodes("b") %>% 
+  html_text()
+
+theDates <- updates %>% 
+  html_nodes("p+ p") %>% 
+  html_text()
+
+info <- updates %>% 
+  html_nodes("br+ p") %>% 
+  html_text()
+
+packages <- character()
+updates <- character()
+titles <- character()
+
+
+for(i in  seq_along(package)) {
+  print(i)
+  s <-str_split(package[[i]],"Package ")[[1]][2]
+  packages[i] <- str_split(s,"  updated")[[1]][1]
+  
+}
+
+for(i in  seq_along(theDates)) {
+  print(i)
+  
+  l <- str_length(theDates[[i]])
+  updates[i] <- str_sub(theDates[[i]],l-11,l-2)
+}
+
+
+
+for(i in  seq_along(info)) {
+  print(i)
+  s <-str_split(info[[i]],"le:  ")[[1]][2]
+  titles[i] <- str_split(s," Description: ")[[1]][1]
+}
+
+
+u <- data.frame(Package=packages,Description=titles,Updated=updates, stringsAsFactors=FALSE)
+
+up <- u$Package
+today <- max(df$date) #class(today) # not sure why 3rd aug
+# names(df_all)
+# library(dplyr) # think needed this in
+# glimpse(df_all)
+
+updates <-df %>% 
+  filter(package %in% up&date==today) %>% 
+  arrange(desc(count)) %>% 
+  inner_join(u, by=c("package"="Package"))
+ # DT::datatable(class='compact stripe hover row-border order-column',rownames=FALSE,options= list(paging = FALSE, searching = FALSE,info=FALSE))
+
+
+print(glimpse(updates))
+
+write_csv(updates,"data/updates.csv")
+
+
+# summarize outside markdown as it takes a while --------------------------
+
+
+
+allByDay <-df %>% 
+  group_by(package) %>% 
+  arrange(date) %>% 
+  slice(1) %>% 
+  group_by(date) %>% 
+  tally() %>% 
+  arrange(date) %>% 
+  mutate(tot=cumsum(n)) %>% 
+  filter(date>="2012-11-01")
+
+write_csv(allByDay,"data/allByDay.csv")
+
+
 library(shinyapps)
+library(beepr)
+
 deployDoc("cranlogsFlexDB.Rmd")
+beep(9)
